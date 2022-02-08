c893f5a092c1eb012c1e2df13f701c54fb2b340b
Pavel Homanau <pavel.homanau@innowise-group.com>
2022-02-08 22:59:43 +0300

    Add dumpdata.

--- TradePlatform/entrypoint.sh
+++ TradePlatform/entrypoint.sh
@@ -4,4 +4,6 @@ set -ex
 #mypy TradePlatform/
 #pylint TradePlatform/
 python3 manage.py migrate
+python3 manage.py flush --noinput
+python3 manage.py dumpdata dump_data.json
 python3 manage.py runserver 0.0.0.0:8000
